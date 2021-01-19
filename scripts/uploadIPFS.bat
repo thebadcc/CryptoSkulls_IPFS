5bb26755b87d83ade0259ebb7bba6bfe001c30eb
thebadcc <thebadcc@outlook.com>
2021-01-19 15:25:19 -0500

    Update uploadIPFS.bat

--- scripts/uploadIPFS.bat
+++ scripts/uploadIPFS.bat
@@ -1,1 +1,2 @@
-
+curl -X POST "https://imageserver.link/upload/image/file" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "image_file=@[File Location]" >> [Response Location]
+timeout /t 7 & echo. >> [Response Location].
